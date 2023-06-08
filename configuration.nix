849a89ed04bf47a6e6eeb2ac93a89cc8a5367417
e <e>
2023-06-08 12:56:31 -0700

    updates

--- configuration.nix
+++ configuration.nix
@@ -250,30 +250,30 @@
         firefox = {
           executable = "${pkgs.firefox-bin}/bin/firefox";
           profile = "${pkgs.firejail}/etc/firejail/firefox.profile";
-          extraArgs = {
+          extraArgs = [
             # Required for U2F USB stick
             "--ignore=private-dev"
             # Enforce dark mode
             "--env=GTK_THEME=Adwaita:dark"
             # Smooth scrolling
-            "--env=MOZ_USE_XINPUT2=1";
+            "--env=MOZ_USE_XINPUT2=1"
             # Use wayland
-            "--env=MOZ_ENABLE_WAYLAND=1";
+            "--env=MOZ_ENABLE_WAYLAND=1"
             # Make chromium use wayland
-            "--env=NIXOS_OZONE_WL=1";
+            "--env=NIXOS_OZONE_WL=1"
             # Enable system notifications
             "--dbus-user.talk=org.freedesktop.Notifications"
-          };
+          ];
         };
         chromium = {
           executable = "${pkgs.chromium}/bin/chromium --enable-features=UseOzonePlatform --ozone-platform=wayland";
           profile = "${pkgs.firejail}/etc/firejail/chromium.profile";
-          extraArgs = {
+          extraArgs = [
             # Enforce dark mode
             "--env=GTK_THEME=Adwaita:dark"
             # Enable system notifications
             "--dbus-user.talk=org.freedesktop.Notifications"
-          };
+          ];
         };
       };
     };
